cc3062a94f6b59b6e5d695e7b2aa97e2f15dbd7c
CleberEstacio <cleberCursoEstacio@gmail.com>
2025-12-06 13:52:46 +0000

    implementado cadastro das CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
@@ -6,10 +7,45 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char carta1, carta2, estado;
+  char codigo_carta, cidade[30];
+  int populacao;
+  int pontos_turisticos;
+  float area, pib;
   // Área para entrada de dados
+  printf("=========Cadastrar cartas==========\n");
+
+  printf("Cadastre a sua carta: \n");
+
+  printf("carta: ");
+  scanf("%c", &carta1);
+
+  printf("Estado: ");
+  scanf("%c\n", &estado);
+
+  printf("Codigo da carta: ");
+  scanf("%s\n", &codigo_carta );
+
+  printf("Cidade: ");
+  scanf("%s\n", &cidade);
+
+  printf("Populacao: \n");
+  scanf("%i", &populacao);
+  
+  printf("Area: \n");
+  scanf("%f", &area);
+  
+  printf("Pib: \n");
+  scanf("%f", &pib);
+
+  printf("Numero de pontos turisticos: \n");
+  scanf("%d", &pontos_turisticos);
+  
+
+  
+
+
 
   // Área para exibição dos dados da cidade
-
-return 0;
+  
 } 
