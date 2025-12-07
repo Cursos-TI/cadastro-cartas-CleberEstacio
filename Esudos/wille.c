193b86bbb172e316eb0d86d81e5f2dd7ce6038f6
CleberEstacio <cleberCursoEstacio@gmail.com>
2025-12-07 16:55:20 -0300

    estrutura de repeticao while em C

---
+++ Esudos/wille.c
@@ -0,0 +1,13 @@
+//Estrutura de repeticao
+#include <stdio.h>
+
+int main() {
+    int inicio = 1;
+
+    while (inicio <=5)
+    {
+        printf("%d\n", inicio);
+        inicio++;
+    }
+    
+}
