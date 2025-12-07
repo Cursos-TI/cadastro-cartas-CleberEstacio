8efc91b00762989e66a6344f33f4479155510d09
CleberEstacio <cleberCursoEstacio@gmail.com>
2025-12-07 17:30:19 -0300

    estrutura de repeticao do_wille

---
+++ Esudos/do_wille.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//estudo da estrutura de repetição do-while
+int main() {
+    int i = 1;
+
+    do{
+        printf("%d\n", i++);
+        
+    } while (i <= 10);
+
+    
+    
+}
