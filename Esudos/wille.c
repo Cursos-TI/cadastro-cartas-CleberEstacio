61ea965af57eb978dc2d33ee567c010930149919
CleberEstacio <cleberCursoEstacio@gmail.com>
2025-12-07 17:23:54 -0300

    novo commit

--- Esudos/wille.c
+++ Esudos/wille.c
@@ -3,11 +3,12 @@
 
 int main() {
     int inicio = 1;
+    
 
-    while (inicio <=5)
+    while (inicio <= 20 )
     {
         printf("%d\n", inicio);
-        inicio++;
+        inicio +=2;
     }
     
 }
